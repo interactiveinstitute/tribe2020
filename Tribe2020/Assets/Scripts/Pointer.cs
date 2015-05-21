baa9c7b7dbfbdf5ce7291676c04ac870b91e8b83
Erik Einebrant <spam.erik@einebrant.se>
2015-05-21 23:31:47 +0200

    Added my own scene for testing, in it you can add and remove blocks to a grid. Next step is to think about heat simulation in this grid and what type of properties objects in these block positions can have in regard to that simulation.

---
+++ Tribe2020/Assets/Scripts/Pointer.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pointer : MonoBehaviour {
+	public Collider groundPlane;
+	public GameObject ground;
+	public Transform markerObject;
+	public GameObject block;
+	public ArrayList blocks;
+
+	// Use this for initialization
+	void Start(){
+		blocks = new ArrayList();
+		ground = (GameObject)GameObject.Find ("ent_ground");
+		groundPlane = ground.GetComponent<Collider> ();
+	}
+
+	void Update(){
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		//Position marker according to grid
+		if(groundPlane.Raycast(ray, out hit, 10000.0f)){
+			markerObject.position = ray.GetPoint(hit.distance);
+			Vector3 pos = markerObject.position;
+			pos.x = Mathf.Floor(pos.x / 5) * 5;
+			pos.z = Mathf.Floor(pos.z / 5) * 5;
+			markerObject.position = pos;
+		}
+
+		//If mouse click, add or delete block depending on if space vacant
+		if(Input.GetMouseButtonDown(0)){
+			GameObject colBlock = CollidesWithBlock();
+
+			if(colBlock == null){
+				GameObject newBlock =
+					(GameObject)Instantiate(block, markerObject.transform.position, Quaternion.identity);
+				blocks.Add(newBlock);
+			} else{
+				blocks.Remove(colBlock);
+				Destroy(colBlock);
+			}
+		}
+	}
+
+	private GameObject CollidesWithBlock(){
+		Collider col = markerObject.GetComponent<Collider> ();
+		Bounds pb = col.bounds;
+		foreach(GameObject b in blocks){
+			Bounds bb = b.GetComponent<Collider>().bounds;
+			if(pb.Intersects(bb)){
+				return b;
+			}
+		}
+		return null;
+	}
+}
