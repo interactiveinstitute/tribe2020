b2a841a6a539dd7e5498b068d3e40d81df15cd9e
Anton Gustafsson <anton.gustafsson@tii.se>
2016-01-27 11:20:38 +0100

    Viewpoint idea

---
+++ Tribe2020/Assets/Scripts/ViewPoint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewPoint : MonoBehaviour {
+
+	public bool HideFloor1;
+	public bool HideFloor2;
+	public bool HideFloor3;
+
+	public bool HideNorthWall;
+	public bool HideWestWall;
+	public bool HideSouthWall;
+
+	public string Name;
+
+	public GameObject Selected;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
