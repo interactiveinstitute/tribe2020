a6a11ecb68f863fe02234bd69cab2edde7a8d0f8
Anton Gustafsson <anton.gustafsson@tii.se>
2016-03-09 11:06:58 +0100

    random

---
+++ Tribe2020/Assets/Scripts/Singelton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Singelton : MonoBehaviour {
+
+
+	//Singleton functionality 
+	private static Singelton _instance;
+
+	void Awake () {
+		_instance = this;
+	}
+
+	public static Singelton GetInstance () {
+		return _instance;
+	}
+
+
+}
