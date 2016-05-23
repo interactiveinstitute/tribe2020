e0698df6fab8454f6e95f32d6d12a90dc8fce420
Belking <johan@belking.se>
2016-05-23 17:03:44 +0200

    Added a Child with standard assets AI
    Added a Child with standard assets AI, Male and female has 3rd person
    control (used Ethans standard assets animations, just removed the run
    animation)

---
+++ Tribe2020/Assets/Art/Models/Avatars/Sit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sit : MonoBehaviour {
+
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponentInChildren<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("f"))
+        {
+            anim.SetInteger("Sit", 1);
+        }
+        else
+        {
+            anim.SetInteger("Sit", 0);
+        }
+
+    }
+}
