1703d5d3b8c6924909d275cb09bd38401da43853
Erik Einebrant <spam.erik@einebrant.se>
2016-06-20 16:07:23 +0200

    Possibility to play video on android and ios. Also scene for doing this using the intro video from populous 3 as place holder.

---
+++ Tribe2020/Assets/Scripts/System/Scenes/MovieController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MovieController : Controller {
+	private CustomSceneManager _sceneMgr;
+
+	public GameObject videoObject;
+	private MediaPlayerCtrl _videoController;
+
+	// Use this for initialization
+	void Start () {
+		_sceneMgr = CustomSceneManager.GetInstance();
+
+		if(videoObject != null) {
+			_videoController = videoObject.GetComponent<MediaPlayerCtrl>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(videoObject != null) {
+
+			//Video is Over
+			if(_videoController.GetCurrentState() == MediaPlayerCtrl.MEDIAPLAYER_STATE.END) {
+				_sceneMgr.LoadScene("MenuScene");
+			}
+		}
+	}
+}
