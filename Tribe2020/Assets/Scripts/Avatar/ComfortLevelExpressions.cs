905557d67c7017279774d2031d0aa833c35f79f0
Belking <johan@belking.se>
2016-12-06 11:41:06 +0100

    Added Cartoon Character prefab with facial expressions + testscript for facials
    press key u to change facial expression

---
+++ Tribe2020/Assets/Scripts/Avatar/ComfortLevelExpressions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComfortLevelExpressions : MonoBehaviour {
+	public GameObject face;
+	public Texture[] myTextures = new Texture[5];
+	int maxTextures;
+	int arrayPos = 0;
+
+	void Start ()
+	{
+		maxTextures = myTextures.Length-1;
+	}
+
+
+	void Update ()
+	{
+		ChangeFaceTexture ();
+	}
+		
+	void ChangeFaceTexture(){
+		if (Input.GetKeyDown (KeyCode.U)) {
+			face.GetComponent<Renderer> ().material.mainTexture = myTextures [arrayPos];
+
+			if (arrayPos == maxTextures) {
+				arrayPos = 0;
+			} else {
+				arrayPos++;
+			}
+
+		} 
+	}
+		
+}
