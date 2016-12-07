2573fbb3651b6542fabb498934307c92cd910e30
Gunnar Oledal <gunnar.oledal@gmail.com>
2016-12-07 16:36:13 +0100

    Merge branch 'master' of github.com:interactiveinstitute/tribe2020

---
+++ Tribe2020/Assets/Scripts/Narration Manager/NarrationStepAttribute.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Struct, Inherited = true)]
+public class NarrationStepAttribute : PropertyAttribute {
+	//The name of the bool field that will be in control
+	public string parameter = "";
+	public bool isTitle = false;
+	////TRUE = Hide in inspector / FALSE = Disable in inspector 
+	////public bool HideInInspector = false;
+
+	public NarrationStepAttribute(string parameter, bool isTitle) {
+		this.parameter = parameter;
+		this.isTitle = isTitle;
+		//this.HideInInspector = false;
+	}
+
+	public NarrationStepAttribute(string parameter) {
+		this.parameter = parameter;
+		//this.HideInInspector = false;
+	}
+
+	//public NarrationStepAttribute(string conditionalSourceField, bool hideInInspector) {
+	//	this.ConditionalSourceField = conditionalSourceField;
+	//	this.HideInInspector = hideInInspector;
+	//}
+}
