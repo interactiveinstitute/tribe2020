23ce0661cf886d6df8d8dc7708524562db89ed96
Erik Einebrant <spam.erik@einebrant.se>
2017-01-25 15:39:01 +0100

    And supposedly some more stuff of varying importance

---
+++ Tribe2020/Assets/Scripts/System/TestController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour {
+	public Narrative theNarrative;
+
+	public enum Limitation {
+		EMPTY, OKPressed, Swiped, Tapped, ApplianceSelected, ApplianceDeselected, QuestListOpened,
+		QuestListClosed, QuestOpened, MeasurePerformed, FindView, AvatarArrived, AvatarSessionOver, AvatarActivityOver,
+		ResourceHarvested, BattleOver, InspectorOpened, InspectorClosed, InboxOpened, InboxClosed, MailOpened, MailClosed,
+		OpenEnergyPanel, CloseEnergyPanel, OpenComfortPanel, CloseComfortPanel, LightSwitchedOff, LightSwitchedOn, AvatarSelected,
+		SelectedOverview, SelectedGridView, CameraMoveOver, CameraAnimationEvent
+	}
+
+	public int updateCount = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		updateCount++;
+		if(updateCount == 2) {
+			theNarrative.steps[0].unityEvent.Invoke();
+		}
+	}
+
+	//
+	public void PlaySound(string sound) {
+		Debug.Log("PlaySound." + sound);
+	}
+
+	//
+	public void PlayAnimation(string animation) {
+		Debug.Log("PlayAnimation." + animation);
+	}
+
+	//
+	public void ShowMessage(string message) {
+		Debug.Log("ShowMessage." + message);
+	}
+
+	//
+	public void LimitInteraction(string limitation) {
+		Debug.Log(limitation.ToString());
+	}
+}
