c36ac1a42dec51f991ae94abb0d3f42fcda7d988
Martin Törnros <martin@interaktivarum.se>
2017-02-08 15:02:48 +0100

    General model office worker, see Slack for functionality

---
+++ Tribe2020/Assets/Scripts/Avatar/AvatarModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarModel : MonoBehaviour {
+
+    public GameObject model;
+    public Avatar avatar;
+
+	// Use this for initialization
+	void Awake () {
+        GameObject goModel = Instantiate(model);
+        Destroy(goModel.GetComponent<Animator>());
+        goModel.name = "Model";
+        SetLayerRecursive(goModel.transform);       
+        goModel.transform.parent = transform;
+        goModel.transform.localPosition = Vector3.zero;
+
+        //Set animator rig
+        Animator animator = gameObject.GetComponent<Animator>();
+        animator.avatar = avatar;
+    }
+
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void SetLayerRecursive(Transform transform) {
+        transform.gameObject.layer = LayerMask.NameToLayer("Avatars");
+        foreach (Transform child in transform) {
+            SetLayerRecursive(child);
+        }
+    }
+
+}
