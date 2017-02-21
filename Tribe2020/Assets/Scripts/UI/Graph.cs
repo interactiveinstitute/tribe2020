1c86a6e94abcc559621dd477cc7fd21df3de1cf0
Martin Törnros <martin@interaktivarum.se>
2017-02-21 14:08:04 +0100

    Memory leak fix in Graph class

--- Tribe2020/Assets/Scripts/UI/Graph.cs
+++ Tribe2020/Assets/Scripts/UI/Graph.cs
@@ -32,12 +32,22 @@ public class Graph : DataNode {
 
 	public List<DataPoint> Data;
 
-	GameTime _timeMgr = GameTime.GetInstance();
+    Mesh msh = new Mesh();
+
+    GameTime _timeMgr = GameTime.GetInstance();
 
 	// Use this for initialization
 	void Start () {
 		_timeMgr = GameTime.GetInstance();
-	}
+
+        Material pMat = new Material(_material);
+        pMat.SetColor("_TintColor", color);
+        pMat.SetColor("_Color", color);
+
+        CanvasRenderer pCanvasRenderer = GetComponent<CanvasRenderer>();
+        pCanvasRenderer.SetMaterial(pMat, null);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -256,19 +266,10 @@ public class Graph : DataNode {
 		}
 
 		//Create the mesh
-		Mesh msh = new Mesh();
 		msh.vertices = vertices3d;
 		msh.triangles = indices;
 		msh.RecalculateNormals();
 		msh.RecalculateBounds();
-
-		//_material = Resources.Load("BaseMaterial") as Material;
-		Material pMat = new Material(_material);
-		pMat.SetColor("_TintColor", color);
-		pMat.SetColor("_Color", color);
-
-		CanvasRenderer pCanvasRenderer = GetComponent<CanvasRenderer>();
-		pCanvasRenderer.SetMaterial(pMat, null);
 
 		RectTransform pRectTransform = GetComponent<RectTransform>();
 		if(pRectTransform == null) {
@@ -282,8 +283,9 @@ public class Graph : DataNode {
 			pRectTransform.localScale = scale;
 		}
 
-		//pCanvasRenderer.Clear();
-		pCanvasRenderer.SetMesh(msh);
+        //pCanvasRenderer.Clear();
+        CanvasRenderer pCanvasRenderer = GetComponent<CanvasRenderer>();
+        pCanvasRenderer.SetMesh(msh);
 
 	}
 }
