1c49ced48acdb6b6d6a0c47ce97abef72a72a708
Anton04 <anton.gustafsson@tii.se>
2017-03-13 10:26:47 +0100

    Changed acopalyse scale

--- Tribe2020/Assets/Scripts/UI/Acopalypsemeter.cs
+++ Tribe2020/Assets/Scripts/UI/Acopalypsemeter.cs
@@ -5,27 +5,43 @@ using UnityEngine.UI;
 
 public class Acopalypsemeter : MonoBehaviour {
 
-    public GameObject Percent, Pointer;
+    //public GameObject Percent, Pointer;
     ResourceManager Resources;
 
-    Text percent_text;
+    public Text PercentText;
+    public RectTransform Pointer;
+    public double change;
+    public double direction;
 
- 
 
-       
-       
+
 
 
     // Use this for initialization
     void Start () {
         Resources = ResourceManager.GetInstance();
-        percent_text = Percent.GetComponent<Text>();
-
+      
     }
 	
 	// Update is called once per frame
 	void Update () {
-        percent_text.text = (Resources.CO2Change * 100).ToString("N0") + "%";
+        double change_capped = change;
+        change = Resources.CO2Change -1;
+        PercentText.text = (change  * 100).ToString("N0") + "%";
 
+        //Capp values
+        if (change_capped > 0.05)
+            change_capped = 0.05;
+        else if (change_capped < -0.57)
+            change_capped = -0.57;
+
+        //More than 20% savings
+        if (change_capped < -0.2)
+            direction = -1*(((change_capped + 0.5) / 0.3) * 90) - 90-180;
+        else
+            direction = -1* (((change_capped + 0.2) / 0.2) * 90);
+
+        
+        Pointer.localEulerAngles = (int)direction  * Vector3.forward;
     }
 }
