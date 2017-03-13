48ab4c38fe486917221d01898fd1e746a17b1c64
Anton04 <anton.gustafsson@tii.se>
2017-03-13 09:10:51 +0100

    Reset pointer to energy buffers in resource manager

---
+++ Tribe2020/Assets/Scripts/UI/Acopalypsemeter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Acopalypsemeter : MonoBehaviour {
+
+    public GameObject Percent, Pointer;
+    ResourceManager Resources;
+
+    Text percent_text;
+
+ 
+
+       
+       
+
+
+    // Use this for initialization
+    void Start () {
+        Resources = ResourceManager.GetInstance();
+        percent_text = Percent.GetComponent<Text>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        percent_text.text = (Resources.CO2Change * 100).ToString("N0") + "%";
+
+    }
+}
