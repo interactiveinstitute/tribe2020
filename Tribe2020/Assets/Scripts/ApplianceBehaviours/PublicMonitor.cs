1a8b19483f6806371444c788778a56ef34b4bb8e
Martin Törnros <martin@interaktivarum.se>
2017-03-14 17:01:01 +0100

    Color TV

---
+++ Tribe2020/Assets/Scripts/ApplianceBehaviours/PublicMonitor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PublicMonitor : MonoBehaviour {
+
+    public Material screenMaterial;
+
+    private MeshRenderer renderer;
+    float timeLastChange;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponentInChildren<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GetComponent<ElectricDevice>().Power > 0.0f) {
+            if (Time.time - timeLastChange > 1) {
+                renderer.materials[1].color = new Color(Random.value, Random.value, Random.value);
+                timeLastChange = Time.time;
+            }
+        }
+    }
+}
