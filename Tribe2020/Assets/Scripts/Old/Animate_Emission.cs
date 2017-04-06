10855882d56200f752b70afaad8c5943ddc688fa
Belking <johan@belking.se>
2017-04-06 14:22:39 +0200

    updates Azucarera

---
+++ Tribe2020/Assets/Scripts/Old/Animate_Emission.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate_Emission : MonoBehaviour {
+
+	public Color colorStart = Color.red;
+	public Color colorEnd = Color.green;
+	public float duration = 1.0F;
+	private Renderer rend;
+	//private Material[] myMaterials;
+	void Start() {
+
+		rend = GetComponent<Renderer>();
+
+	}
+
+	void Update() {
+		foreach (Material mat in rend.materials) {
+
+					// mat.color = Color.red;
+					float lerp = Mathf.PingPong(Time.time, duration) / duration;
+					mat.SetColor("_EmissionColor", Color.Lerp(colorStart, colorEnd, lerp));
+					}			
+
+	}
+
+}
+
---
+++ Tribe2020/Assets/Scripts/Old/Animate_all_renderers.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate_all_renderers : MonoBehaviour {
+
+	public float colorStart = 1f;
+	public float colorEnd = 0f;
+	public float duration = 1.0F;
+	public Renderer[] AllRenderers;
+	private Renderer rend;
+	//private Material[] myMaterials;
+	void Start() {
+		AllRenderers = (Renderer[])Resources.FindObjectsOfTypeAll(typeof(Renderer));
+		rend = GetComponent<Renderer>();
+	}
+
+
+
+	void Update() {
+		
+		foreach (Renderer rend in AllRenderers) {
+			foreach (Material mat in rend.materials) {
+
+				// mat.color = Color.red;
+				float lerp = Mathf.PingPong (Time.time, duration) / duration;
+				mat.SetFloat ("_Metallic", Mathf.Lerp (colorStart, colorEnd, lerp));
+			}			
+
+		}
+	}
+}
+/*
+Renderer[] arrend = (Renderer[])Resources.FindObjectsOfTypeAll(typeof(Renderer));
+foreach (Renderer rend in arrend)
+{
+	foreach (Material mat in rend.sharedMaterials)
+	{
+		if (!armat.Contains(mat))
+		{
+			armat.Add(mat);
+		}
+	}
+}
+*/
