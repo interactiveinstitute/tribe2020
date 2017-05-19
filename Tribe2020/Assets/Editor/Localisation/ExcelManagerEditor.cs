5ed83b7b35b31ccf382083b9ba7d1334da63ef51
Erik Einebrant <spam.erik@einebrant.se>
2017-05-19 18:02:12 +0200

    TRIBE specialization of excel exporter

---
+++ Tribe2020/Assets/Editor/Localisation/ExcelManagerEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ExcelManager))]
+public class ExcelManagerEditor : Editor {
+
+	//Add elements while drawing the inspector
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		ExcelManager script = (ExcelManager)target;
+
+		if(GUILayout.Button("Export to .xls", GUILayout.Width(150))) {
+			script.ExportAllExcels();
+		}
+	}
+}
