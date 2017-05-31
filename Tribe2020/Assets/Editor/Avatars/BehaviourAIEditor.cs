2f9870116dadd1173ad9ea63670e40882bcbb0c1
Erik Einebrant <spam.erik@einebrant.se>
2017-05-31 11:41:11 +0200

    Temp editor button for copying schedule to schedules[0], requires schedules to have size of >0

---
+++ Tribe2020/Assets/Editor/Avatars/BehaviourAIEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BehaviourAI))]
+public class BehaviourAIEditor : Editor {
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		//Set dirty, in order to save new data on game start
+		BehaviourAI ai = (BehaviourAI)target;
+		EditorUtility.SetDirty(ai);
+
+		if(GUILayout.Button("CopyOldSchedule", GUILayout.Width(150))) {
+			ai.schedules[0].items = ai.schedule;
+		}
+	}
+}
