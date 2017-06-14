40dd21c78903d3dd1e5464d2949e2b67fe828ecc
Anton Gustafsson <anton.gustafsson@tii.se>
2017-06-14 13:06:17 +0200

    Editor for server reapplied.

---
+++ Tribe2020/Assets/Editor/AppServerEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(AppServer))]
+public class AppServerEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		AppServer myScript = (AppServer)target;
+
+
+		if(GUILayout.Button("Connect",GUILayout.Width(100) ))
+		{
+			myScript.Start();
+		}
+
+		if(GUILayout.Button("Request",GUILayout.Width(100) ))
+		{
+			myScript.GetPeriod("test/signalA",double.NaN,double.NaN,null);
+		}
+			
+	}
+}
