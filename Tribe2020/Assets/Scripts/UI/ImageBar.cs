2b68c35e7220c8931f3018d1c738542cc686bb4f
Erik Einebrant <spam.erik@einebrant.se>
2017-06-22 21:37:56 +0200

    Added new graphical bar for battle scene

---
+++ Tribe2020/Assets/Scripts/UI/ImageBar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageBar : MonoBehaviour {
+	public int value;
+	private int _lastValue = 0;
+	public Sprite sprite;
+	public Color tint;
+	//public GameObject imagePrefab;
+
+	// Use this for initialization
+	void Start () {
+		foreach(Transform child in transform) {
+			Destroy(child.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_lastValue != value) {
+			if(value > _lastValue) {
+				for(int i = value - _lastValue; i > 0; i--) {
+					GameObject newImgGO = Instantiate(new GameObject(), transform);
+					Image newImg = newImgGO.AddComponent<Image>();
+					newImg.sprite = sprite;
+					newImg.color = tint;
+					newImg.preserveAspect = true;
+				}
+			} else {
+				for(int i = _lastValue - value; i > 0; i--) {
+					Destroy(transform.GetChild(i).gameObject);
+				}
+			}
+			_lastValue = value;
+		}
+	}
+}
