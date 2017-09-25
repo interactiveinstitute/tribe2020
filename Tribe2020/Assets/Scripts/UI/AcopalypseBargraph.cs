2d3c3fab859b80b03aeaee2ec1582cffe7c4d89b
Erik Einebrant <spam.erik@einebrant.se>
2017-09-25 12:36:06 +0200

    Tweaked canvas prefab and apocabargraph-script to support responsive ui , also sending with controller prefab in case, but it mostly have better defaults where ouezygin is not excluded by default

--- Tribe2020/Assets/Scripts/UI/AcopalypseBargraph.cs
+++ Tribe2020/Assets/Scripts/UI/AcopalypseBargraph.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class AcopalypseBargraph : MonoBehaviour {
-	
-	public SingleBar [] Bars ;
+	public SingleBar[] Bars;
 
 	public double[] Outcomes;
 	public double[] Baselines;
@@ -14,10 +13,10 @@ public class AcopalypseBargraph : MonoBehaviour {
 	public DataSeries Baseline;
 
 	public double UpdateTime = 0;
-	public double UpdateIntervall = 5;
+	public double UpdateIntervall = 1;
 
 	public double MaxValue = 0;
-	public double VScale = 10000;
+	public double VScale = 10000 * 0.1f;
 	public double height = 200;
 
 	public double EaseStart = 0;
@@ -29,59 +28,52 @@ public class AcopalypseBargraph : MonoBehaviour {
 	public double EaseDebug = 0;
 
 	GameTime time;
-	public Text [] newText ;
-
+	public Text[] newText;
 
 	// Use this for initialization
-	void Start () {
-
-		if (Outcome == null)
+	void Start() {
+		if(Outcome == null)
 			Outcome = DataContainer.GetInstance().cO2Outcome;
-		if (Baseline == null)
+		if(Baseline == null)
 			Baseline = DataContainer.GetInstance().cO2Baseline;
 
 		Outcomes = new double[7];
 		Baselines = new double[7];
 
+		//Bars = GetComponentsInChildren<SingleBar>();
 
-		Bars = GetComponentsInChildren<SingleBar> ();
+		UpdateValues();
 
-		UpdateValues ();
+		newText = GetComponentsInChildren<Text>();
+		time = GameTime.GetInstance();
 
-		newText = GetComponentsInChildren<Text> ();
-		time = GameTime.GetInstance ();
-
-		UpdateLegend ();
-
+		UpdateLegend();
 	}
-		
-	
-	// Update is called once per frame
-	void Update () {
 
+	// Update is called once per frame
+	void Update() {
 		double now = Time.unscaledTime;
 
-		//AutoScale (now);
+		AutoScale (now);
 
-		if (now - UpdateTime < UpdateIntervall)
+		if(now - UpdateTime < UpdateIntervall)
 			return;
 
 		UpdateTime = now;
 
-		UpdateValues ();
-		CalculateMax ();
-		UpdateLegend ();
-		UpdateBars ();
-
+		UpdateValues();
+		CalculateMax();
+		UpdateLegend();
+		UpdateBars();
 	}
 
-	void AutoScale(double now){
-
-		if (MaxValue == VScale)
+	//
+	void AutoScale(double now) {
+		if(MaxValue == VScale)
 			return;
 
 		//Init new ease?
-		if (EaseTarget != MaxValue) {
+		if(EaseTarget != MaxValue) {
 			EaseStart = now;
 			EaseDiff = MaxValue - VScale;
 			EaseStartValue = VScale;
@@ -90,86 +82,78 @@ public class AcopalypseBargraph : MonoBehaviour {
 
 		double delta = now - EaseStart;
 
-		if (delta > EaseDuration)
-		{
+		if(delta > EaseDuration) {
 			EaseStart = 0;
 			VScale = EaseTarget;
 			return;
 		}
-			
-		EaseDebug = ParametricBlend (delta / EaseDuration);
 
-		VScale = EaseStartValue + EaseDiff * ParametricBlend (delta / EaseDuration);
-	
+		EaseDebug = ParametricBlend(delta / EaseDuration);
+
+		VScale = EaseStartValue + EaseDiff * ParametricBlend(delta / EaseDuration);
 	}
 
-	 	 	
 	//Quadratic ease out where t = time, b = startvalue, c = change in value, d = duration:
-
-	double ParametricBlend(double t)
-	{
-		double sqt = t*t;
+	double ParametricBlend(double t) {
+		double sqt = t * t;
 		return sqt / (2.0 * (sqt - t) + 1.0);
 	}
 
-
+	//
 	void UpdateValues() {
-		for (int i = 0; i < 7; i++) {
-			Outcomes [i] = Outcome.InterpolateDailyConsumption (i*-1);
+		for(int i = 0; i < 7; i++) {
+			Outcomes[i] = Outcome.InterpolateDailyConsumption(i * -1);
 		}
-		for (int i = 0; i < 7; i++) {
-			Baselines [i] = Baseline.InterpolateDailyConsumption (i*-1);
-		}
-	}
-
-	void UpdateBars(){
-		for (int i = 0; i<7;i++) {
-			if (i > Bars.Length)
-				break;
-			
-			Bars[i].EaseTo = Outcomes [i] / MaxValue;
-
-		}
-
-		for (int i = 0; i<7;i++) {
-			if (i > Bars.Length)
-				break;
-
-			Bars[i+7].EaseTo = Baselines [i] / MaxValue;
-
+		for(int i = 0; i < 7; i++) {
+			Baselines[i] = Baseline.InterpolateDailyConsumption(i * -1);
 		}
 	}
 
+	//
+	void UpdateBars() {
+		for(int i = 0; i < 7; i++) {
+			if(i > Bars.Length)
+				break;
+
+			Bars[i].EaseTo = Outcomes[i] / MaxValue;
+		}
+
+		for(int i = 0; i < 7; i++) {
+			if(i > Bars.Length)
+				break;
+
+			Bars[i + 7].EaseTo = Baselines[i] / MaxValue;
+		}
+	}
+
+	//
 	void CalculateMax() {
+		double max = 0;
 
-		double max=0;
+		for(int i = 0; i < 7; i++) {
 
-		for (int i = 0; i < 7; i++) {
-
-			if (Baselines [i] > max)
-				max = Baselines [i];
+			if(Baselines[i] > max)
+				max = Baselines[i];
 		}
 
-		for (int i = 0; i < 7; i++) {
+		for(int i = 0; i < 7; i++) {
 
-			if (Outcomes [i] > max)
-				max = Outcomes [i];
+			if(Outcomes[i] > max)
+				max = Outcomes[i];
 		}
 
 		MaxValue = max;
 	}
 
-
-
-	void UpdateLegend(){
-
-		newText [0].text = time.GetDay (-6);
-		newText [1].text = time.GetDay (-5);
-		newText [2].text = time.GetDay (-4);
-		newText [3].text = time.GetDay (-3);
-		newText [4].text = time.GetDay (-2);
-		newText [5].text = "Yesterday";
-		newText [6].text = "Today";
+	//
+	void UpdateLegend() {
+		newText[0 + 3].text = time.GetDay(-6);
+		newText[1 + 3].text = time.GetDay(-5);
+		newText[2 + 3].text = time.GetDay(-4);
+		newText[3 + 3].text = time.GetDay(-3);
+		newText[4 + 3].text = time.GetDay(-2);
+		newText[5 + 3].text = "Yesterday";
+		newText[6 + 3].text = "Today";
 
 	}
 }
