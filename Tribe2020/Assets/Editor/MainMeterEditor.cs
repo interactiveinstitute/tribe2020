a70b8154aae3dc13dc6113bf101f738ca64fc4fe
Anton Gustafsson <anton.gustafsson@tii.se>
2017-11-20 16:53:56 +0100

    Graph container

---
+++ Tribe2020/Assets/Editor/MainMeterEditor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MainMeter))]
+public class MainMeterEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		MainMeter myScript = (MainMeter)target;
+
+
+		if(GUILayout.Button("Switch On",GUILayout.Width(100) ))
+		{
+			myScript.On();
+		}
+
+		if(GUILayout.Button("Switch Off",GUILayout.Width(100) ))
+		{
+			myScript.Off();
+		}
+
+		if(GUILayout.Button("Toggle",GUILayout.Width(100) ))
+		{
+			myScript.Toggle();
+		}
+
+		if(GUILayout.Button("Reset",GUILayout.Width(100) ))
+		{
+			myScript.reset_energy();
+
+		}
+
+
+		if(GUILayout.Button("Connect",GUILayout.Width(100) ))
+		{
+			myScript.Connect(myScript.PowerSource);
+		}
+
+
+
+	}
+}
---
+++ Tribe2020/Assets/GraphContainer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphContainer : MonoBehaviour {
+
+	public Graph[] graphs; 
+	public double Min,Max;
+	public double DataMin, DataMax;
+	public bool AutoMax = true,AutoMin= false;
+
+	public enum ScaleTypeEnum {SameScale, MixedScale};
+
+	public ScaleTypeEnum ScaleType;
+
+
+	// Use this for initialization
+	void Start () {
+		graphs = gameObject.GetComponentsInChildren<Graph>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FindMinMax ();
+
+		Ease ();
+			
+	}
+
+	void FindMinMax(){
+
+		double min = double.PositiveInfinity, max = double.NegativeInfinity;
+		
+		foreach (Graph graph in graphs) {
+			if (graph.DataMin < min)
+				min = graph.DataMin;
+
+			if (graph.DataMax > max)
+				max = graph.DataMax;
+
+		}
+
+		DataMin = min;
+		DataMax = max;
+
+		if (AutoMax)
+			Max = DataMax;
+	}
+
+	void Ease(){
+
+		if (ScaleType == ScaleTypeEnum.SameScale)
+		foreach (Graph graph in graphs) {
+				graph.SetMax (Max);
+		}
+
+	}
+
+}
