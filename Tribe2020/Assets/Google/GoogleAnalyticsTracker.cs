1709ae14502996369e03f983cc1c15df0fd5af13
Martin Törnros <martin@interaktivarum.se>
2018-01-19 12:16:14 +0100

    Google Analyitcs preparation

---
+++ Tribe2020/Assets/Google/GoogleAnalyticsTracker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoogleAnalyticsTracker : MonoBehaviour {
+
+    public GoogleAnalyticsV4 googleAnalytics;
+    public string screenName;
+
+    // Use this for initialization
+    void Start () {
+        googleAnalytics.StartSession();
+        googleAnalytics.LogScreen(screenName);
+        googleAnalytics.DispatchHits();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDestroy() {
+        googleAnalytics.Dispose();
+    }
+}
